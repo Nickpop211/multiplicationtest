44e9c6d1fc1b4e205ec1fdebd871e3c8406a8dba
Nickpop211 <94858590+Nickpop211@users.noreply.github.com>
2023-07-30 08:12:33 -0600

    Update simple multiplication.py
    I have modified this script to check user inputs for numbers less than 0 and more then 10.

--- simple multiplication.py
+++ simple multiplication.py
@@ -1,6 +1,17 @@
 import random
-num_of_ques = 10
-num_of_ques = int(input("Number of questions to ask ? "))
+#Checks user input to avoid program crash
+while True:
+	try:
+		num_of_ques = int(input("Number of questions to ask ? "))
+		if 1 <= num_of_ques <=10:
+			break
+		elif num_of_ques < 0:
+			print("Number can not be negative")
+		else:
+			print("Number is to high. Please input a number from 1 - 10")
+	except ValueError:
+		print("Invalid Character. Please input a number from 1 - 10")
+		
 bigset = []
 score = 0
 first = second = [1,2,3,4,5,6,7,8,9,10]
@@ -8,8 +19,7 @@ for i in first:
 	for j in second:
 		#populate all possible queations in a list for random selection
 		bigset.append([i,j])
-while num_of_ques > len(bigset):
-	num_of_ques = int(input("Please reduce the number of questions: "))
+
 #select random questions from all available options
 ques=random.sample(bigset,num_of_ques)
 
@@ -18,7 +28,7 @@ print("\nAnswer the following:\n")
 for qloop in range (0,len):
 	firstnum = ques[qloop][0]
 	secondnum = ques[qloop][1]
-	ans = input(str(qloop+1) + ") " + str(firstnum) + " x " + str(secondnum) + " = ")	
+	ans = input(str(qloop+1) + ") " + str(firstnum) + " x " + str(secondnum) + " = ")
 	if ans == str(firstnum * secondnum):
 		score=score +1
 print("\nScore = " + str(score) + "/" + str(num_of_ques))
